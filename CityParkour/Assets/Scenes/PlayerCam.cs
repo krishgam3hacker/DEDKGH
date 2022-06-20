466850cb90e6a47972f88e93a56bdd19719fb398
krishgamehacker <krishgamehacker@gmail.com>
2022-06-20 17:23:56 +0530

    updateee

--- CityParkour/Assets/Scenes/PlayerCam.cs
+++ CityParkour/Assets/Scenes/PlayerCam.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PlayerCam : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float sensX;
+    public float sensY;
+
+    public Transform orientation;
+
+    float xRotation;
+    float yRotation;
+
+    private void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        yRotation += mouseX;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+
+
     }
+
+
 }
