34f4a495b3acee4d4b25aa7c31fbb46a36d9837c
Master KGH <krishgamehacker@gmail.com>
2023-03-31 17:51:51 +0530

    [Systems] Door System

---
+++ TheForgottenAsylum/Assets/Scripts/AdvancedDoors.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdvancedDoors : MonoBehaviour
+{
+
+    public Animator door;
+    public GameObject lockOB;
+    public GameObject keyOB;
+    public GameObject openText;
+    public GameObject closeText;
+    public GameObject lockedText;
+
+
+    public AudioSource openSound;
+    public AudioSource closeSound;
+    public AudioSource lockedSound;
+    public AudioSource unlockedSound;
+
+    private bool inReach;
+    private bool doorisOpen;
+    private bool doorisClosed;
+    public bool locked;
+    public bool unlocked;
+
+
+
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Reach" && doorisClosed)
+        {
+            inReach = true;
+            openText.SetActive(true);
+        }
+
+        if (other.gameObject.tag == "Reach" && doorisOpen)
+        {
+            inReach = true;
+            closeText.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            openText.SetActive(false);
+            lockedText.SetActive(false);
+            closeText.SetActive(false);
+        }
+    }
+
+    void Start()
+    {
+        inReach = false;
+        doorisClosed = true;
+        doorisOpen = false;
+        closeText.SetActive(false);
+        openText.SetActive(false);
+    }
+
+
+
+
+    void Update()
+    {
+        if (lockOB.activeInHierarchy)
+        {
+            locked = true;
+            unlocked = false;
+        }
+
+        else
+        {
+            unlocked = true;
+            locked = false;
+        }
+
+        if (inReach && keyOB.activeInHierarchy && Input.GetButtonDown("Interact"))
+        {
+            unlockedSound.Play();
+            locked = false;
+            keyOB.SetActive(false);
+            StartCoroutine(unlockDoor());
+        }
+
+        if (inReach && doorisClosed && unlocked && Input.GetButtonDown("Interact"))
+        {
+            door.SetBool("Open", true);
+            door.SetBool("Closed", false);
+            openText.SetActive(false);
+            openSound.Play();
+            doorisOpen = true;
+            doorisClosed = false;
+        }
+
+        else if (inReach && doorisOpen && unlocked && Input.GetButtonDown("Interact"))
+        {
+            door.SetBool("Open", false);
+            door.SetBool("Closed", true);
+            closeText.SetActive(false);
+            closeSound.Play();
+            doorisClosed = true;
+            doorisOpen = false;
+        }
+
+        if (inReach && locked && Input.GetButtonDown("Interact"))
+        {
+            openText.SetActive(false);
+            lockedText.SetActive(true);
+            lockedSound.Play();
+        }
+
+    }
+
+    IEnumerator unlockDoor()
+    {
+        yield return new WaitForSeconds(.05f);
+        {
+
+            unlocked = true;
+            lockOB.SetActive(false);
+        }
+    }
+
+
+
+
+}
---
+++ TheForgottenAsylum/Assets/Scripts/PickUpKey.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpKey : MonoBehaviour
+{
+    public GameObject keyOB;
+    public GameObject invOB;
+    public GameObject pickUpText;
+    public AudioSource keySound;
+
+    public bool inReach;
+
+
+    void Start()
+    {
+        inReach = false;
+        pickUpText.SetActive(false);
+        invOB.SetActive(false);
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = true;
+            pickUpText.SetActive(true);
+
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            pickUpText.SetActive(false);
+
+        }
+    }
+
+
+    void Update()
+    {
+        if (inReach && Input.GetButtonDown("Interact"))
+        {
+            keyOB.SetActive(false);
+            keySound.Play();
+            invOB.SetActive(true);
+            pickUpText.SetActive(false);
+        }
+
+        
+    }
+}
