20115509d5d4a2a3a70173e16b87bd93312b07e0
Master KGH <krishgamehacker@gmail.com>
2023-04-01 23:21:35 +0530

    [Game System] Light Flickering
    -Test Box Anim Fix

---
+++ TheForgottenAsylum/Assets/Scripts/LightsFlicker.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightsFlicker : MonoBehaviour
+{
+    public Light lightOB;
+
+    public AudioSource lightSound;
+
+    public float minTime;
+    public float maxTime;
+    public float timer;
+
+
+    void Start()
+    {
+        timer = Random.Range(minTime, maxTime);
+        
+    }
+
+
+
+
+    void Update()
+    {
+        LightsFlickering();
+    }
+
+    void LightsFlickering()
+    {
+        if (timer > 0)
+            timer -= Time.deltaTime;
+
+        if(timer <= 0)
+        {
+            lightOB.enabled = !lightOB.enabled;
+            timer = Random.Range(minTime, maxTime);
+            lightSound.Play();
+        }
+
+
+
+    }
+}
