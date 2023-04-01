57a70e61739dbbbdd0ead228ae4ba2f84a20208e
Master KGH <krishgamehacker@gmail.com>
2023-04-01 17:59:32 +0530

    [Player] HUD

---
+++ TheForgottenAsylum/Assets/Scripts/HUD.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUD : MonoBehaviour
+{
+    public GameObject flashLightON;
+    public GameObject flashLightOFF;
+    public GameObject flashLightOB;
+
+
+
+
+
+
+    void Start()
+    {
+        flashLightON.SetActive(false);
+        
+    }
+
+
+
+
+    void Update()
+    {
+        if(flashLightOB.activeInHierarchy)
+        {
+            flashLightON.SetActive(true);
+            flashLightOFF.SetActive(false);
+        }
+        
+        else
+        {
+            flashLightON.SetActive(false);
+            flashLightOFF.SetActive(true);
+        }
+
+    }
+}
