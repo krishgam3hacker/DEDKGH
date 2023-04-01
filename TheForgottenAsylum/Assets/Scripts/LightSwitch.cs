dc5b97d7a990dd93e4b6e0eb3ebed60f7de9cf4c
Master KGH <krishgamehacker@gmail.com>
2023-04-01 19:09:40 +0530

    [Player] Light Switch System

---
+++ TheForgottenAsylum/Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour
+{
+    public GameObject onOB;
+    public GameObject offOB;
+
+    public GameObject lightsText;
+
+
+    public GameObject lightOB;
+
+
+    public AudioSource switchClick;
+
+    public bool lightsAreOn;
+    public bool lightsAreOff;
+    public bool inReach;
+
+
+    void Start()
+    {
+        inReach = false;
+        lightsAreOn = false;
+        lightsAreOff = true;
+        onOB.SetActive(false);
+        offOB.SetActive(true);
+        lightOB.SetActive(false);
+    }
+
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = true;
+            lightsText.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            lightsText.SetActive(false);
+        }
+    }
+
+
+
+    void Update()
+    {
+        if(lightsAreOn && inReach && Input.GetButtonDown("Interact"))
+        {
+            lightOB.SetActive(false);
+            onOB.SetActive(false);
+            offOB.SetActive(true);
+            switchClick.Play();
+            lightsAreOff = true;
+            lightsAreOn = false;
+        }
+
+        else if (lightsAreOff && inReach && Input.GetButtonDown("Interact"))
+        {
+            lightOB.SetActive(true);
+            onOB.SetActive(true);
+            offOB.SetActive(false);
+            switchClick.Play();
+            lightsAreOff = false;
+            lightsAreOn = true;
+        }
+
+
+    }
+}
