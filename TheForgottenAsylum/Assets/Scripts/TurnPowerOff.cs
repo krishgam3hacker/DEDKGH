766cad5e260869ff831bbcd2c0d4acf60d70e954
Master KGH <krishgamehacker@gmail.com>
2023-04-06 22:02:35 +0530

    [Game System] Breaker System

---
+++ TheForgottenAsylum/Assets/Scripts/TurnPowerOff.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnPowerOff : MonoBehaviour
+{
+    private GameObject player;
+
+    private GameObject breakerBox;
+
+    public bool destroyAfterUse;
+
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        breakerBox = GameObject.Find("BreakerBox");
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+
+            if (destroyAfterUse)
+            {
+                breakerBox.GetComponent<TurnPowerOn>().powerIsOn = false;
+                Destroy(gameObject);
+            }
+
+            if (!destroyAfterUse)
+            {
+                breakerBox.GetComponent<TurnPowerOn>().powerIsOn = false;
+            }
+        }
+    }
+}
---
+++ TheForgottenAsylum/Assets/Scripts/TurnPowerOn.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnPowerOn : MonoBehaviour
+{
+    public GameObject[] lights;
+
+    public GameObject text;
+
+    public bool powerIsOn;
+
+    private bool inReach;
+
+    void Start()
+    {
+
+        foreach (GameObject ob in lights)
+        {
+            ob.SetActive(false);
+        }
+
+        text.SetActive(false);
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Reach" && !powerIsOn)
+        {
+            inReach = true;
+            text.SetActive(true);
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            text.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Interact") && inReach)
+        {
+            powerIsOn = true;
+        }
+
+        if(powerIsOn)
+        {
+            foreach (GameObject ob in lights)
+            {
+                ob.SetActive(true);
+            }
+
+            inReach = false;
+            text.SetActive(false);
+        }
+
+        if (!powerIsOn)
+        {
+            foreach (GameObject ob in lights)
+            {
+                ob.SetActive(false);
+            }
+
+        }
+
+    }
+}
