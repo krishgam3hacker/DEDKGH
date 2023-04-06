6354a4986e7bf77e219fbfc3e54502f7fffe0d80
Master KGH <krishgamehacker@gmail.com>
2023-04-06 16:59:44 +0530

    [Game Systems] Compass System

---
+++ TheForgottenAsylum/Assets/Scripts/CompassImage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompassImage : MonoBehaviour
+{
+    public Transform player;
+    Vector3 dir;
+
+    void Start()
+    {
+        
+    }
+
+
+
+    void Update()
+    {
+        dir.z = player.eulerAngles.y;
+        transform.localEulerAngles = dir;
+    }
+}
