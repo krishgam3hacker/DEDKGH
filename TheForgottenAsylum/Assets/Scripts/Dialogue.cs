3046f7fe3a5d3e17eea19cdf58c3d0e1c00faf87
Master KGH <krishgamehacker@gmail.com>
2023-04-06 18:11:10 +0530

    [Game System] Dialogues

---
+++ TheForgottenAsylum/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textOB;
+    public GameObject Activator;
+    public string dialogue = "Dialogue";
+
+    public float timer = 2f;
+
+
+
+    void Start()
+    {
+        textOB.GetComponent<TextMeshProUGUI>().enabled = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            textOB.GetComponent<TextMeshProUGUI>().enabled = true;
+            textOB.text = dialogue.ToString();
+            StartCoroutine(DisableText());
+        }
+    }
+
+    IEnumerator DisableText()
+    {
+        yield return new WaitForSeconds(timer);
+        textOB.GetComponent<TextMeshProUGUI>().enabled = false;
+        Destroy(Activator);
+
+    }
+
+
+}
